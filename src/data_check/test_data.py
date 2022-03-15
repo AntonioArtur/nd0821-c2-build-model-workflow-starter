7f3100a6999f67e7b4bb7ce5b8b231f794ca1fca
Artur <antonio.moura@dellead.com>
2022-03-15 12:09:05 -0300

    Done

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -63,3 +63,14 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 ########################################################
 # Implement here test_row_count and test_price_range   #
 ########################################################
+def test_row_count(data):
+    """
+    Tests if number of row is according with what is expected.
+    """
+    assert 15000 < data.shape[0] < 1000000
+
+def test_price_range(data, min_price, max_price):
+    """
+    Tests if price tag is in a reasonable range.
+    """
+    assert data.price.between(min_price, max_price).all()
